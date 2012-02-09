721d52e46aeff910ca1fd27eaefef5c9c4d37bd7
Riccardo Iaconelli <riccardo@kde.org>
2012-02-09 13:36:49 +0100

    a couple of cosmetic changes for testing
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- main.cpp
+++ main.cpp
@@ -9,17 +9,11 @@ int main(int argc, char **argv)
     v1.setX(3);
     v1.setY(2);
     v1.setZ(1);
-    v1.dump();
-//     v2 = (v1*3);
-//     v2.setY(3);
     v2.setX(1);
     v2.setY(2);
     v2.setZ(3);
-    v2.dump();
     
-//     if (v2 == (v1*3)) {
-   Vector::cross(v2,v1).normalize().dump();
-//     }
+   Vector::cross(v1,v2).dump();
     
     std::cout << "Hello, world!" << std::endl;
     return 0;
