21b8d3170c7ccf2fc5b26b5573407a5c4928f1da
Riccardo Iaconelli <riccardo@kde.org>
2012-02-16 23:04:50 +0100

    round of updates. BREAKS STUFF
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

---
+++ universebatch.cpp
@@ -0,0 +1,89 @@
+/*
+    Copyright (C) 2012  Riccardo Iaconelli <riccardo@kde.org>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "universebatch.h"
+
+UniverseBatch::UniverseBatch()
+{
+//     std::cout << "New thread" << endl;
+//     m_thread(&UniverseBatch::do_work, this);
+}
+
+UniverseBatch::~UniverseBatch()
+{
+//     m_thread.join();
+}
+
+void UniverseBatch::setObstacleList(std::list< Obstacle::Ptr > list)
+{
+    m_obstacles = list;
+}
+
+void UniverseBatch::setParticleList(Particle::List list)
+{
+    m_particles = list;
+}
+void UniverseBatch::setBoundary(real boundary)
+{
+    m_boundary = boundary;
+}
+void UniverseBatch::setDeltaT(int deltat)
+{
+    m_deltat = deltat;
+}
+
+void UniverseBatch::do_work()
+{
+/*
+    while (!m_particles.empty()) {
+        m_stepCount++;
+        for(Particle::List::iterator it = m_particles.begin(); it != m_particles.end(); ++it) {
+            if ((*it).alive()) {
+                moveParticle(*it);
+            }
+
+            if(!(*it).alive()) {
+//                 std::cout << "Deleting a particle" << std::endl;
+                it = m_particles.erase(it);
+            }
+        }
+    }*/
+    // Thread finished: add another thread and detach
+}
+
+void UniverseBatch::moveParticle(Particle& particle)
+{
+//     if (particle.position().abs() > m_boundary) {
+//         particle.absorb(); // Kill the particle
+//         return;
+//     }
+// 
+//     Vector deltax = particle.speed()*m_deltat;
+// 
+//     particle.move(m_deltat);
+//     Vector newPos = particle.position();
+// 
+//     foreach(const Obstacle::Ptr obstacle, m_obstacles) {
+//         if (obstacle->contains(newPos)) {
+//             obstacle->tryAbsorb(particle, deltax.abs());
+//         }
+// //         if (!particle.alive()) {
+// //             break; // Avoid absorbing a particle multiple times? how do you handle colliding objects?
+// //         }
+//     }
+}
---
+++ universebatch.h
@@ -0,0 +1,52 @@
+/*
+    Copyright (C) 2012  Riccardo Iaconelli <riccardo@kde.org>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef UNIVERSEBATCH_H
+#define UNIVERSEBATCH_H
+
+#include "particle.h"
+#include "obstacle.h"
+#include <boost/thread.hpp>
+
+class UniverseBatch
+{
+
+public:
+    UniverseBatch();
+    virtual ~UniverseBatch();
+    void setParticleList(Particle::List list);
+    void setObstacleList(std::list<Obstacle::Ptr> list);
+
+    void setDeltaT(int deltat);
+    void setBoundary(real boundary);
+    
+
+private:
+    void do_work();
+    void moveParticle(Particle &particle);
+
+    boost::thread m_thread;
+    int m_deltat;
+    real m_boundary;
+    
+    std::list<Obstacle::Ptr> m_obstacles;
+    Particle::List m_particles;
+    
+};
+
+#endif // UNIVERSEBATCH_H
